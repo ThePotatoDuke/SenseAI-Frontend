de3b3e4117b52b9c58a4cf395e19853adc68317c
Mustafa <gameacedamy@gmail.com>
2025-03-03 16:18:50 +0300

    removed open filex code

--- lib/features/user_auth/presentation/pages/home_page.dart
+++ lib/features/user_auth/presentation/pages/home_page.dart
@@ -346,52 +346,6 @@ class _HomePageState extends State<HomePage> with WidgetsBindingObserver {
     }
   }
 
-  // void _handleMessageTap(BuildContext _, types.Message message) async {
-  //   if (message is types.FileMessage) {
-  //     var localPath = message.uri;
-  //
-  //     if (message.uri.startsWith('http')) {
-  //       try {
-  //         final index =
-  //             _messages.indexWhere((element) => element.id == message.id);
-  //         final updatedMessage =
-  //             (_messages[index] as types.FileMessage).copyWith(
-  //           isLoading: true,
-  //         );
-  //
-  //         setState(() {
-  //           _messages[index] = updatedMessage;
-  //         });
-  //
-  //         final client = http.Client();
-  //         final request = await client.get(Uri.parse(message.uri));
-  //         final bytes = request.bodyBytes;
-  //         final documentsDir = (await getApplicationDocumentsDirectory()).path;
-  //         localPath = '$documentsDir/${message.name}';
-  //
-  //         if (!File(localPath).existsSync()) {
-  //           final file = File(localPath);
-  //           await file.writeAsBytes(bytes);
-  //         }
-  //       } finally {
-  //         final index =
-  //             _messages.indexWhere((element) => element.id == message.id);
-  //         final updatedMessage =
-  //             (_messages[index] as types.FileMessage).copyWith(
-  //           isLoading: null,
-  //         );
-  //
-  //         setState(() {
-  //           _messages[index] = updatedMessage;
-  //         });
-  //       }
-  //     }
-  //
-  //     await OpenFilex.open(localPath);
-  //   }
-  // }
-
-
 
   void _handleMessageTap(BuildContext context, types.Message message) async {
     if (message is types.FileMessage) {
