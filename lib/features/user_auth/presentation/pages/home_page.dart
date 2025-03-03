c1866c71a2886887dd1f988cac7616c69b5a846f
Mustafa <gameacedamy@gmail.com>
2025-03-03 14:20:20 +0300

    used flutter pub get and updated files

--- lib/features/user_auth/presentation/pages/home_page.dart
+++ lib/features/user_auth/presentation/pages/home_page.dart
@@ -12,7 +12,7 @@ import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
 import 'package:flutter_chat_ui/flutter_chat_ui.dart';
 import 'package:http/http.dart' as http;
 import 'package:image_picker/image_picker.dart';
-import 'package:open_filex/open_filex.dart';
+// import 'package:open_filex/open_filex.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:record/record.dart';
 import 'package:senseai/features/user_auth/presentation/pages/video_screen.dart';
@@ -159,7 +159,7 @@ class _HomePageState extends State<HomePage> with WidgetsBindingObserver {
               child: Chat(
                 messages: _messages,
                 onAttachmentPressed: _handleAttachmentPressed,
-                onMessageTap: _handleMessageTap,
+                // onMessageTap: _handleMessageTap,
                 onPreviewDataFetched: _handlePreviewDataFetched,
                 onSendPressed: _handleSendPressed,
                 user: _user,
@@ -345,50 +345,50 @@ class _HomePageState extends State<HomePage> with WidgetsBindingObserver {
     }
   }
 
-  void _handleMessageTap(BuildContext _, types.Message message) async {
-    if (message is types.FileMessage) {
-      var localPath = message.uri;
+  // void _handleMessageTap(BuildContext _, types.Message message) async {
+  //   if (message is types.FileMessage) {
+  //     var localPath = message.uri;
 
-      if (message.uri.startsWith('http')) {
-        try {
-          final index =
-              _messages.indexWhere((element) => element.id == message.id);
-          final updatedMessage =
-              (_messages[index] as types.FileMessage).copyWith(
-            isLoading: true,
-          );
+  //     if (message.uri.startsWith('http')) {
+  //       try {
+  //         final index =
+  //             _messages.indexWhere((element) => element.id == message.id);
+  //         final updatedMessage =
+  //             (_messages[index] as types.FileMessage).copyWith(
+  //           isLoading: true,
+  //         );
 
-          setState(() {
-            _messages[index] = updatedMessage;
-          });
+  //         setState(() {
+  //           _messages[index] = updatedMessage;
+  //         });
 
-          final client = http.Client();
-          final request = await client.get(Uri.parse(message.uri));
-          final bytes = request.bodyBytes;
-          final documentsDir = (await getApplicationDocumentsDirectory()).path;
-          localPath = '$documentsDir/${message.name}';
+  //         final client = http.Client();
+  //         final request = await client.get(Uri.parse(message.uri));
+  //         final bytes = request.bodyBytes;
+  //         final documentsDir = (await getApplicationDocumentsDirectory()).path;
+  //         localPath = '$documentsDir/${message.name}';
 
-          if (!File(localPath).existsSync()) {
-            final file = File(localPath);
-            await file.writeAsBytes(bytes);
-          }
-        } finally {
-          final index =
-              _messages.indexWhere((element) => element.id == message.id);
-          final updatedMessage =
-              (_messages[index] as types.FileMessage).copyWith(
-            isLoading: null,
-          );
+  //         if (!File(localPath).existsSync()) {
+  //           final file = File(localPath);
+  //           await file.writeAsBytes(bytes);
+  //         }
+  //       } finally {
+  //         final index =
+  //             _messages.indexWhere((element) => element.id == message.id);
+  //         final updatedMessage =
+  //             (_messages[index] as types.FileMessage).copyWith(
+  //           isLoading: null,
+  //         );
 
-          setState(() {
-            _messages[index] = updatedMessage;
-          });
-        }
-      }
+  //         setState(() {
+  //           _messages[index] = updatedMessage;
+  //         });
+  //       }
+  //     }
 
-      await OpenFilex.open(localPath);
-    }
-  }
+  //     // await OpenFilex.open(localPath);
+  //   }
+  // }
 
   void _handlePreviewDataFetched(
     types.TextMessage message,
