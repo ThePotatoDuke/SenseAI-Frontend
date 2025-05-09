084e23b2913ea7a08b4e4b242f1ff9f92fb4a7b9
ThePotatoDuke <gameacedamy@gmail.com>
2025-05-09 13:20:19 +0300

    added new dependencies

---
+++ lib/features/user_auth/data/data_service.dart
@@ -0,0 +1,7 @@
+import 'package:permission_handler/permission_handler.dart';
+Future<void> requestStoragePermission() async {
+  final status = await Permission.storage.request();
+  if (!status.isGranted) {
+    throw Exception("Storage permission not granted");
+  }
+}
